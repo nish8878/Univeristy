e13feb8d19e5e1ae05d5de08dad294fd1a937834
Aakriti Sharma <aakriti.sharma18@siesgst.ac.in>
2020-12-03 21:33:02 +0530

    nav changed

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMSubtreeModified', function (e) {
+    if(e.target.tagName === 'HTML' && window.google) {
+            if(e.target.className.match('translated')) {
+                document.header.style.backgroundPosition="0px 40px";
+            } else {
+                document.header.style.backgroundPosition="0px 0px";
+            }
+        }
+    }, true);
